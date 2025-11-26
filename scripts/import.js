a9666b1f961a5d8717b9275360ae620c09859d9f
Efitz <malorie.filliane@gmail.com>
2025-11-26 21:54:38 +0100

    display default guide
    Default guide displayed in guide viewer.
    Not formatted but data can be fetch from lua file

--- scripts/import.js
+++ scripts/import.js
@@ -5,22 +5,46 @@ fetch(fileRead)
   	.then(response => response.text())
   	.then((data) => {
 		splitData = data.toString().split('\r\n')
-
-		
-		let ul = document.getElementById("guide-list-display");
+		let ul = document.getElementById("guide-list-display")
 		let display = "Guide List"
 		for (let index = 0; index < splitData.length; index++) {
-			let element = splitData[index];
-			element = element.split("\\")
+			let fullPath = splitData[index]
+			let formatedPath = '\"\\guides\\'+fullPath+'\"'
+
+			let element = fullPath.split("\\")
 			if (element[0] != null && element[0].length > 1) {
 				if (element[2]) {
-					display += "<li class='list-group-item guide-list-element'>"+element[2]+"</li>"
+					display += "<li class='list-group-item guide-list-element' onclick='loadGuide("+formatedPath+")'>"+element[2]+"</li>"
 				} else {
-					display += "<li class='list-group-item guide-list-element'>"+element[1]+"</li>"
+					display += "<li class='list-group-item guide-list-element' onclick='loadGuide("+formatedPath+")'>"+element[1]+"</li>"
 				}
 			}
 		}	
-		ul.innerHTML += display;
+		ul.innerHTML += display
   	})
 
 //Display guide when you click
+function loadGuide(fullPath) {
+	if (fullPath == "default") {
+		fullPath = "\\guides\\TUGs\\Alliance\\1-9Dwarf&Gnome.lua"
+	}
+
+	let guideViewer = document.getElementById("guide-viewer")
+	let display = ''
+
+	fetch(fullPath)
+	.then(response => response.text())
+	.then((data) => {
+		splitData = data.toString().split('\r\n')
+		for (let index = 0; index < splitData.length; index++) {
+			const element = splitData[index];
+			if (index > 2 && index < (splitData.length-1)) {
+				display += '<div class="col-12">'+element+'</div>'
+				console.log(element)
+			}
+			
+		}
+		guideViewer.innerHTML = display
+		// console.log(splitData)
+	})
+}
